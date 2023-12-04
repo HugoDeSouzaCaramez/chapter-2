ba31da5c056908c11279be2c0291841bc3655b35
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2023-12-04 20:02:13 -0300

    Reutilizando componentes no React parte 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import logo from './assets/logo.svg';
+import Profile from './pages/Profile';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header logo={logo} />
+      <Profile userName="hugo.7.musica@gmail.com" />
     </div>
   );
 }
