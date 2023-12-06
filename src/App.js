601745c38444da4b41a9dedd2a8b638332bd73a2
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2023-12-06 00:57:36 -0300

    Roteamento com react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import logo from './assets/logo.svg';
 import Profile from './pages/Profile';
 import './App.css';
+import Projects from './pages/Projects';
+import ProjectDetail from './pages/ProjectDetail';
 
 function App() {
   return (
     <div className="App">
-      <Header logo={logo} />
-      <Profile userName="hugo.7.musica@gmail.com" />
+      <BrowserRouter>
+        <Header logo={logo} />
+        <Routes>
+          <Route
+            path="/"
+            element={<Profile userName="hugo.7.musica@gmail.com" />}
+          />
+          <Route
+            path="/projects"
+            element={<Projects userName="hugo.7.musica@gmail.com" />}
+          />
+          <Route
+            path="/projects/:name"
+            element={<ProjectDetail userName="hugo.7.musica@gmail.com" />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
